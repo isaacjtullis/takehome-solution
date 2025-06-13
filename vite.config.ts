a90603a7b8fd04f4839ea50a3ecdbbca2eb365c0
Isaac Tullis <isaactullis@Isaacs-Air.lan>
2025-06-12 20:18:23 -0400

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,16 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom'],
+          'tiptap': ['@tiptap/core', '@tiptap/react', '@tiptap/starter-kit'],
+          'ui': ['@radix-ui/react-dropdown-menu', '@radix-ui/react-navigation-menu', '@radix-ui/react-popover', '@radix-ui/react-tooltip']
+        }
+      }
+    }
+  }
 });
